27b9ecc4976813e7371b4739af8e7ad5ba62b417
R@MS <sramsiks@gmail.com>
2021-10-29 10:31:04 +0300

    move js code to separate files

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -72,78 +72,9 @@
         </div>
     </div>
 
-
-
-
 @endsection
 
 
 @section('scripts')
-    <script>
-
-        /**
-         * delete one selected employee
-         * @param employeeId
-         */
-        function deleteEmployee(employeeId) {
-            console.log(employeeId);
-            $.ajax({
-                url: window.location.pathname + '/' + employeeId,
-                type: 'DELETE',
-                data: {_token: $('[name=_token]').val()}
-            }).done(function (data) {
-                console.log(data);
-                if (data.error === false) {
-                    toastr.success(data.msg);
-                    window.location.reload();
-                } else {
-                    toastr.warning(data.msg);
-                }
-            }).fail(function (error) {
-                console.log(error);
-                if (error.status === 403) {
-                    toastr.warning('This action is unauthorized.');
-                } else {
-                    toastr.warning('Problem with your request ');
-                }
-            });
-        }
-
-        /**
-         * save employee data
-         */
-        function saveEmployee() {
-            console.log('save');
-            let postData = {
-                _token: $('[name=_token]').val(),
-                email: $('#employeeEmail').val(),
-                password: $('#employeePassword').val(),
-            };
-            console.log(postData);
-
-            $.post(window.location.pathname, postData)
-                .done(function (data) {
-                    console.log(data);
-                    if (data.error === false) {
-                        toastr.success(data.msg);
-                        window.location.reload();
-                    } else {
-                        toastr.warning(data.msg);
-                    }
-                }).fail(function (error) {
-                console.log(error);
-                if (error.responseJSON !== undefined) {
-                    let responseText = error.responseJSON;
-                    let problemField = Object.keys(responseText.errors)[0];
-                    let errorMsg = responseText.errors[problemField][0];
-                    console.log(errorMsg);
-                    toastr.warning('Problem with ' + problemField, errorMsg);
-                } else {
-                    toastr.warning('Problem with your request ');
-                }
-
-            });
-        }
-
-    </script>
+    <script src="/js/employee.js"></script>
 @endsection
